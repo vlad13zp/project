a16b0dcecd9d249bb6f8c43bc84899a42d2b842a
vlad13zp <vlad13zp@yandex.ru>
2015-04-28 08:32:45 +0300

    Change menu and delete memeber class

--- src/todo.py
+++ src/todo.py
@@ -4,15 +4,7 @@ import redis
 import json
 import os
 
-
-class member(object):
-
-	def __init__(self, id, description, deadline, importance):
-		self.id = id
-		self.task_args = dict(description=description,
-								deadline=deadline, importance=importance)
-
-class Todo(member):
+class Todo(object):
 
 	def __init__(self, *args):
 		self.r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
@@ -21,19 +13,19 @@ class Todo(member):
 		for arg in self.args:
 			self.dict_name[arg.id] = arg.task_args
 
-	def delete(self,id):
+	def delete(self):
 		key = False
+		my_id = raw_input('Введите идентификатор задачи : ')
 		for i in self.r.scan_iter():
-			if (i == id):
-				self.r.delete(id)
+			if (i == my_id):
+				self.r.delete(my_id)
 				key = True
 				break
 		if (key):
 			print 'Система : запись успешно удалена!'
-			raw_input()
 		else :
 			print 'Система : записи с таким идентификатором не существует!'
-			raw_input()
+		return True
 
 	def add(self):		
 		val = False		
@@ -51,30 +43,29 @@ class Todo(member):
 			self.r.set(my_id, json.dumps(self.dict_name[my_id]))
 			self.dict_name.pop(my_id)
 			print 'Система : запись успешно добавлена!'
-			raw_input()
 		else : 
 			print 'Система : идентификатор записи не уникален. Будьте внимательны!'
-			raw_input()
+		return True
 
-	def edit(self,id):
+	def edit(self):
 		key = False
+		my_id = raw_input('Введите идентификатор задачи : ')
 		for i in self.r.scan_iter():
-			if (i == id):
+			if (i == my_id):
 				key = True
 				break
 		if (key):
-			self.dict_name[id] = json.loads(self.r.get(id))
-			for i,k in self.dict_name[id].iteritems():
+			self.dict_name[my_id] = json.loads(self.r.get(my_id))
+			for i,k in self.dict_name[my_id].iteritems():
 				des = raw_input(i + ' : ')
-				self.dict_name[id][i] = des
-			self.r.delete(id)
-			self.r.set(id, json.dumps(self.dict_name[id]))
-			self.dict_name.pop(id)
+				self.dict_name[my_id][i] = des
+			self.r.delete(my_id)
+			self.r.set(my_id, json.dumps(self.dict_name[my_id]))
+			self.dict_name.pop(my_id)
 			print 'Система : запись успешно отредактирована!'
-			raw_input()
 		else :
 			print 'Система : записи с таким идентификатором не существует!'
-			raw_input()
+		return True
 
 	def printMe(self):
 		c = 1
@@ -87,7 +78,12 @@ class Todo(member):
 			for v,d in local.iteritems():
 				print '## ' + v + ' : ' + d
 			c = c + 1
+		return True
 
+	def exit(self):
+		key = False
+		print 'До свидания!'
+		return key
 
 def main():
 	a = Todo()
@@ -100,27 +96,16 @@ def main():
 		print '[4] - Вывести список задач'
 		print '[5] - Выход'
 		des = raw_input('Введите желаемую команду : ')
-		if (des == '1'):
-			a.add()
-			os.system('clear')
-		elif (des == '2'):
-			my_id = raw_input('Введите номер задачи : ')
-			a.delete(my_id)
-			os.system('clear')
-		elif (des == '3'):
-			my_id = raw_input('Введите номер задачи : ')
-			a.edit(my_id)
-			os.system('clear')
-		elif (des == '4'):
-			print a.printMe()
-			raw_input()
-			os.system('clear')
-		elif (des == '5'):
-			key = False
-		else : 
-			print 'Система : введено ошибочное значение!'
-			raw_input()
-			os.system('clear')
+		functions = {'1': a.add, 
+             '2': a.delete, 
+             '3': a.edit,
+             '4': a.printMe,
+             '5': a.exit}
+
+		func = functions[des] 
+		key = func()
+		raw_input()
+		os.system('clear')
 
 main()
 
